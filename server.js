2364bda1e83c558daaba4f74ad2905bdb29ed549
YogiboKorea <yogico.design@gmail.com>
2025-02-27 17:15:20 +0900

    version

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const DB_NAME = process.env.DB_NAME;
 const MONGODB_URI = process.env.MONGODB_URI;
 const CAFE24_MALLID = process.env.CAFE24_MALLID;
 const OPEN_URL = process.env.OPEN_URL; // OpenAI API URL
-const CAFE24_API_VERSION = process.env.CAFE24_API_VERSION || '2020-04-01';
+const CAFE24_API_VERSION = process.env.CAFE24_API_VERSION || '2024-06-01';
 
 // Express 앱 초기화
 const app = express();

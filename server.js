d6fbe1721114948da7b326d88eef21999d4d5f43
YogiboKorea <yogico.design@gmail.com>
2025-02-27 11:15:37 +0900

    UPASD

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ async function getOrderInfo(memberId) {
   const params = {
     member_id: memberId,
     start_date: '2024-08-31',
-    end_date: '2025-02-27',
+    end_date: '2024-09-01',
     limit: 10,
   };
   try {

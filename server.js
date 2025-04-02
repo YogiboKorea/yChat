af84d20a963ed6693eec7bf15b99661eff043066
YogiboKorea <yogico.design@gmail.com>
2025-04-02 15:56:03 +0900

    chats

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ const CAFE24_API_VERSION = process.env.CAFE24_API_VERSION || '2024-06-01';
 function convertLinkText(promptText) {
   // [카카오플친 연결하기]를 a 태그로 변환
   const kakaoPattern = /\[카카오플친 연결하기\]/g;
-  const kakaoLink = '<a href="http://pf.kakao.com/_lxmZsxj/chat" target="_blank" rel="noopener noreferrer">카카오플친 연결하기</a>';
+  const kakaoLink = '<a href="http://pf.kakao.com/_lxmZsxj/chat" target="_blank" rel="noopener noreferrer">카카오플친 연결하기12</a>';
   
   // [네이버톡톡 연결하기]를 a 태그로 변환
   const naverPattern = /\[네이버톡톡 연결하기\]/g;
-  const naverLink = '<a href="https://talk.naver.com/ct/wc4u67?frm=psf" target="_blank" rel="noopener noreferrer">네이버톡톡 연결하기</a>';
+  const naverLink = '<a href="https://talk.naver.com/ct/wc4u67?frm=psf" target="_blank" rel="noopener noreferrer">네이버톡톡 연결하기12</a>';
   
   return promptText
     .replace(kakaoPattern, kakaoLink)
@@ -55,8 +55,8 @@ const rawSystemPrompt = `
 아래 JSON 데이터는 참고용 포스트잇 Q&A 데이터입니다. 이 데이터를 참고하여 적절한 답변을 생성해 주세요.
 
 3. 항상 모드 대화의 마지막엔 추가 궁금한 사항이 있으실 경우,  
-[카카오플친 연결하기]  
-[네이버톡톡 연결하기]  
+[카카오플친 연결하기1]  
+[네이버톡톡 연결하기1]  
 라고 안내해 주세요.
 `;
 
